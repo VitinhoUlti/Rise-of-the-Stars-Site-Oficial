673c2b5d10bda5bb822a1052e0ae925a09949f52
Vitor Miguel Santos Redondo <140042669+VitinhoUlti@users.noreply.github.com>
2024-05-25 00:49:49 -0300

    quase pronto
    falta as imagens dos climas

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let anoAtual = document.getElementById("2020s");
-let dataDoComputador = new Date().getFullYear();
+(function ColocarEventos(){
+    const anoAtual = document.getElementById("2020s");
+    let dataDoComputador = new Date().getFullYear();
+    
+    if(dataDoComputador < 1981){
+        anoAtual.style.display = "none";
+    }
+})();
 
-if(dataDoComputador < 1981){
-    anoAtual.style.display = "none";
-}
+(async function AdicionarBloco(){
+    const apiResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=33.00&lon=49.00&appid=ba2acfbf29fe888fea7010dee7d8952e`).catch(erro => console.log(erro));
+    const apiJson = await apiResponse.json();
+
+    const dia = document.createTextNode("Dia: " + new Date().getDate() + "/" + Number(new Date().getMonth() + 1));
+    const descricao = document.createTextNode("Descrição: " + JSON.stringify(apiJson.weather[0].description));
+    const ceu = document.createTextNode("Céu: " + JSON.stringify(apiJson.weather[0].main));
+    const id = document.createTextNode("Id: " + JSON.stringify(apiJson.weather[0].id));
+
+    const diaDiv = document.getElementById("dia");
+    const descricaoDiv = document.getElementById("descricao");
+    const ceuDiv = document.getElementById("céu");
+    const idDiv = document.getElementById("id");
+
+    diaDiv.appendChild(dia);
+    descricaoDiv.appendChild(descricao);
+    ceuDiv.appendChild(ceu);
+    idDiv.appendChild(id);
+})()
